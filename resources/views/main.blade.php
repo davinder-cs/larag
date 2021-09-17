6be4a4f3e14b23165d543c6e892e288af7e07866
example <test@example.com>
2021-09-17 13:33:06 +0530

    init

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,12 +5,16 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- Bootstrap 5 -->
+        <!-- Bootstrap 5.0.2 -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <!-- Font Awesome 4.7 -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- css -->
+        <link href="{{ asset('style.css') }}" rel="stylesheet">
     </head>
     <body class="d-flex flex-column h-100">
-        <nav class="navbar navbar-light navbar-expand-lg" style="background-color: #e3f2fd;">
+        <nav class="navbar navbar-light navbar-expand-lg mb-1" style="background-color: #e3f2fd;">
             <div class="container-fluid">
                 <a class="navbar-brand mr-auto" href="{{ config('app.url') }}">{{ config('app.name') }}</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
@@ -35,13 +39,12 @@
                 </div>
             </div>
         </nav>
-        <main class="main-container">
+        <main class="main-container row">
             @section('sidebar')
-            <div class="sidebar-container">
             @show
+            <div class="d-flex col">
+                @yield('content')
             </div>
-            
-            @yield('content')
         </main>
         <footer class="footer mt-auto py-3 bg-light">
             <div class="container-fluid">
---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,24 @@
+<div class="inner-sidebar d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
+        <ul class="nav nav-pills flex-column mb-auto">
+          <li class="nav-item">
+            <a href="#" class="nav-link active" aria-current="page">
+              <i class="fa fa-home" aria-hidden="true"></i>
+              Home
+            </a>
+          </li>
+        </ul>
+        <hr>
+        <div class="dropdown">
+          <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+            <strong>{{ auth()->user()->name }}</strong>
+          </a>
+          <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+            <li><a class="dropdown-item" href="#">New project...</a></li>
+            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Sign out</a></li>
+          </ul>
+        </div>
+    </div>
